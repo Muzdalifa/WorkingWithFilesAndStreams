63bc45dbe2f7ca74b8d30fa598f21758ac936d80
Muzdalifa <a.muzdalifa27@gmail.com>
2021-07-12 03:39:14 +0200

    Add project files.

---
+++ DataProcessor/FileProcessor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace DataProcessor
+{
+    class FileProcessor
+    {
+        
+        public string InputFilePath { get;}
+
+        public FileProcessor(string filePath) => InputFilePath = filePath;
+    
+        public void Process()
+        {
+            Console.WriteLine($"Begin process of {InputFilePath}");        
+        }
+    }
+}
