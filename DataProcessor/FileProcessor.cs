c9dafb9ddc5588a694354d86b4bb41611ee053aa
Muzdalifa <a.muzdalifa27@gmail.com>
2021-07-12 08:25:39 +0200

    Copy file from one folder(directory) to another.

--- DataProcessor/FileProcessor.cs
+++ DataProcessor/FileProcessor.cs
@@ -5,7 +5,9 @@ namespace DataProcessor
 {
     class FileProcessor
     {
-        
+        private const string BackupDirectoryName = "backup";
+        private const string InProgressDirectoryName = "processing";
+        private const string CompleteDirectoryName = "complete";
         public string InputFilePath { get;}
 
         public FileProcessor(string filePath) => InputFilePath = filePath;
@@ -24,6 +26,30 @@ namespace DataProcessor
             //Getting the parent directory of a path
             string rootDirectoryPath = new DirectoryInfo(InputFilePath).Parent.Parent.FullName;
             Console.WriteLine($"Root data path is {rootDirectoryPath}");
+
+            //Check if backup exist, if its not create new directory(folder)
+            //string backupDirectoryPath = rootDirectoryPath + "/" + BackupDirectoryName; //this is the C# way of concatinating path 
+            string backupDirectoryPath = Path.Combine(rootDirectoryPath, BackupDirectoryName); //Better to use this
+
+            if (!Directory.Exists(backupDirectoryPath))
+            {
+                Console.WriteLine($"Creating {backupDirectoryPath}");
+                Directory.CreateDirectory(backupDirectoryPath); //create folder/directory called backup
+            }
+
+            //Now we have create folder(directory) called backup, we want to copy file data.txt from in to backup : //Copy file to backup dir
+            //get file name component of the original path
+            string inputFileName = Path.GetFileName(InputFilePath);  //this retun filename with extension. To get filename without extension use GetFileNameWithoutExtension() method
+            Console.WriteLine(inputFileName);
+            string backupFilePath = Path.Combine(backupDirectoryPath, inputFileName);
+            Console.WriteLine(backupFilePath);
+            Console.WriteLine($"Copying {InputFilePath} to {backupFilePath}");
+            //copy file (source, distination)
+            //File.Copy(InputFilePath, backupFilePath); //if file is already exist with that name it will return exception, so
+            //copy file even if file already exist overwrite it
+            File.Copy(InputFilePath, backupFilePath, true);
+
+
 
         }
     }
